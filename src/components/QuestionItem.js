45f1ea2c8aaad356bc915e627855db78fdb020e1
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2023-07-18 19:31:11 -0600

    all working deliverables, successful CRUD implementation, failing test for delete request; however working delete on back & front end

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,28 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, handleDeleteQuestion, onAnswerChange }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  function handleDeleteClick () {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE"
+    })
+    handleDeleteQuestion(id)
+  };
+
+  function handleAnswerChange (event) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "correctIndex": event.target.value
+      })
+    })
+    .then(resp => resp.json())
+    .then(answer => onAnswerChange(answer))
+  };
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -15,9 +36,9 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleAnswerChange} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,21 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions, handleDeleteQuestion, onAnswerChange }) {
+
+  const questionDisplay = questions.map(question => {
+    return <QuestionItem 
+              key={question.id} 
+              question={question} 
+              handleDeleteQuestion={handleDeleteQuestion} 
+              onAnswerChange={onAnswerChange} 
+           />
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionDisplay}</ul>
     </section>
   );
 }
